435b81bb60f61af514d5c9361647349620c077f4
Riley Castelli <castelliriley@gmail.com>
2024-01-14 14:47:04 -0500

    update update files

--- update-scripts-shortcuts/update-lethal-extended.bat
+++ update-scripts-shortcuts/update-lethal-extended.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p UserInputPath=What Drive is the game installed to (Letter only)
+set /p UserInputPath=What Drive is the game installed to (Letter only) -- (Stable-Branch Installer)
 echo(Detecting Game Installation Directory ...
 for %%a in (%UserInputPath%) do (
     pushd "%%~a:\"
@@ -12,13 +12,13 @@ echo(Game Installation Directory : %gameInstallationDirectory%
 
 RMDIR /S /Q "C:\update-temp\"
 mkdir C:\update-temp
-curl -L "https://lethal.pengucc.com/releases/Lethal-Extended-Latest.zip" --output "C:\update-temp\Lethal-Extended-Latest.zip"
+curl -L "https://github.com/n-tropy247/NtropicCompany/releases/download/v1.0/release.zip" --output "C:\update-temp\Lethal-Extended-Latest.zip"
 powershell Expand-Archive "C:\update-temp\Lethal-Extended-Latest.zip" -DestinationPath "C:\update-temp"
 powershell -command "Start-Sleep -s 2"
 del /q /s "%gameInstallationDirectory%\BepInEx\*"
 del /s "C:\update-temp\Lethal-Extended-Latest.zip"
 xcopy /e "C:\update-temp" "%gameInstallationDirectory%" /Y
-xcopy /e "C:\update-temp\update-scripts-shortcuts\update-lethal-extended.bat" "C:\%USERPROFILE%\Desktop" /Y
+xcopy /e "C:\update-temp\update-scripts-shortcuts\update-lethal-extended.bat" "%USERPROFILE%\Documents\" /Y
 RMDIR /S /Q "C:\update-temp\"
 echo(Update Finished!
 pause
